30c2b2ee4e2a1d28a1e146bfae84c94276ba7ee3
Shankar Baddi <sbaddi@blueplanet.com>
2022-03-18 18:08:51 +0530

    Revert "addition"
    This reverts commit 474e25eeeda6e54c720288437a0d4a1d894a4be1.

--- src/main/java/Mydemo.java
+++ src/main/java/Mydemo.java
@@ -4,6 +4,5 @@ public class Mydemo {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("hey");
-        System.out.println("hru");
     }
 }
