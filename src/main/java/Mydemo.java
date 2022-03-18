eb521bdd1a71f347bea1ab653f00093c3917659d
Shankar Baddi <sbaddi@blueplanet.com>
2022-03-18 18:09:23 +0530

    hru

--- src/main/java/Mydemo.java
+++ src/main/java/Mydemo.java
@@ -4,5 +4,6 @@ public class Mydemo {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("hey");
+        System.out.println("hru");
     }
 }
